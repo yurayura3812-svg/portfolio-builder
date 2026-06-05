4f403fabab35ed5b13fcc369f218858f55d687d8
yurayura3812-svg <yurayura3812@gmail.com>
2026-06-05 10:39:29 +0900

    config: プロジェクトを静的HTML書き出しモード（output: export）に設定

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // 
-  allowedDevOrigins: ['192.168.11.3', 'localhost:3000']
+  allowedDevOrigins: ['192.168.11.3', 'localhost:3000'],
+  // ビルドしたときにHTML/CSSとして書き出す設定
+  output: 'export'
 };
 
 export default nextConfig;
