a95a900e0f22e40f95cfac4d141d1630840af1b4
yurayura3812-svg <yurayura3812@gmail.com>
2026-06-05 09:56:08 +0900

    fix: ネットワーク越しだと静的サイト状態（ボタンが動かない）になっていたのを解決

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-
+  // 
+  allowedDevOrigins: ['192.168.11.3', 'localhost:3000']
 };
 
 export default nextConfig;
